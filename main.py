3697dece840010938e7be1f6878d03c115aed314
Anna J <ajasion5@gmail.com>
2023-12-11 05:24:34 +0000

    add user exit program

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+
+def user_exit():
+    print("Exititng... Bye!")
+    return
+
+user_info = input('What would you like to do? tape exit or continue ')
+
+if user_info == "exit":
+    user_exit()
+
+
+    
+
+
+
